b74d437de1056425f1e2d97f6a730824f8f84e95
Lan Khuê 1610 <cscd1509@gmail.com>
2025-08-04 09:01:59 +0000

    tôi có làm rõ thêm bối cảnh và 1 số câu hỏi với nhà đầu tư,  bạn tham kh

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Button } from '@/components/ui/button';
--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -1,3 +1,4 @@
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { HardHat, School, Star, Plane } from 'lucide-react';
 import React from 'react';

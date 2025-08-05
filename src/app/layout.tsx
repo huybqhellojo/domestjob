ad4975d737c74a811298b62ffa0e8cef4b79be39
ベトグェン <beto.hanoi@gmail.com>
2025-08-05 02:34:27 +0000

    Montserrat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Playfair+Display:wght@700;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap"
           rel="stylesheet"
         />
       </head>

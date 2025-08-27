3c218870b689477769b9f5b3598bb440f747aa74
Lan Khuê 1610 <cscd1509@gmail.com>
2025-08-27 08:31:23 +0000

    add json

---
+++ src/lib/visas.ts
@@ -0,0 +1,15 @@
+export const VISAS = [
+    { label: 'Thực tập sinh', code: 'TTS' },
+    { label: 'Tokutei', code: 'TKT' },
+    { label: 'Kỹ sư', code: 'KS' }
+]
+export const VISA_DETAILS = [
+    { label: 'Thực tập sinh 3 năm', parent: 'Thực tập sinh', code: '1.1' },
+    { label: 'Thực tập sinh 1 năm', parent: 'Thực tập sinh', code: '1.1' },
+    { label: 'Thực tập sinh 3 go', parent: 'Thực tập sinh', code: '1.1' },
+    { label: 'Tokutei đầu Nhật', parent: 'Tokutei', code: '1.2' },
+    { label: 'Tokutei đầu Việt', parent: 'Tokutei', code: '1.2' },
+    { label: 'Tokutei đi mới', parent: 'Tokutei', code: '1.2' },
+    { label: 'Kỹ sư đầu Nhật', parent: 'Kỹ sư', code: '1.3' },
+    { label: 'Kỹ sư đầu Việt', parent: 'Kỹ sư', code: '1.3' },
+]
